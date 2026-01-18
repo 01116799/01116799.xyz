535f2114bd828f92006ceae913f61eedd5a7cdb7
01116799 <8140349+01116799@users.noreply.github.com>
2026-01-18 22:37:54 +0700

    01116799 blog contents

---
+++ src/consts.ts
@@ -0,0 +1,40 @@
+import type { Metadata, Site, Socials } from "@types";
+
+export const SITE: Site = {
+  TITLE: "01116799",
+  DESCRIPTION: "investment blog",
+  EMAIL: "mail@01116799.xyz",
+  NUM_POSTS_ON_HOMEPAGE: 5,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "investment blog",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "blog",
+  DESCRIPTION: "investment records",
+};
+
+export const PROJECTS: Metadata = {
+  TITLE: "articles",
+  DESCRIPTION:
+    "articles",
+};
+
+export const SOCIALS: Socials = [
+  {
+    NAME: "X (formerly Twitter)",
+    HREF: "",
+  },
+  {
+    NAME: "GitHub",
+    HREF: "",
+  },
+  {
+    NAME: "Website",
+    HREF: "01116799.xyz",
+  },
+];
